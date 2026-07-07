4c4320938cfe00325058dec51c76418f32c7350a
Beshil <ersanbievbesh@gmail.com>
2026-07-07 15:03:41 +0300

    add design tokens & register global styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "../index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
 import AppProviders from "./app/providers";
 
+import "@/app/styles/globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

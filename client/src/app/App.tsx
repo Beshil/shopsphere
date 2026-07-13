9fe9f8a3d12454cc229b1247dd0592a6e55b3dd5
Beshil <ersanbievbesh@gmail.com>
2026-07-13 19:17:21 +0300

    add Container component

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,15 +1,16 @@
+import Container from "@/components/ui/Container";
 import AppRouter from "./AppRouter";
 import { useTheme } from "./providers/ThemeProvider";
 
 function App() {
   const { theme, toggleTheme } = useTheme();
   return (
-    <>
+    <Container>
       <h1>shop</h1>
       <p>current {theme}</p>
       <button onClick={toggleTheme}> toggle</button>
       <AppRouter />
-    </>
+    </Container>
   );
 }
 
